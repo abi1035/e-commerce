d43685c860de39e927e76a068b6e3ed481ce0cf1
Abishek Kenneth <abishek.kenneth@gmail.com>
2024-06-25 17:44:17 -0400

    eslint error fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
-  rules:{"prettier/pretttier":['error',{"endOfLine":"auto"}]
-}
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'off',
+  },
 }
