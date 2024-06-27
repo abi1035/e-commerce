583f3c965db10f8943de5f5e9d2955557210f743
Abishek Kenneth <abishek.kenneth@gmail.com>
2024-06-27 15:44:00 -0400

    Hosting issues

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -11,62 +11,63 @@ import Link from 'next/link'
 import { Button } from '../../Button'
 import { Media } from '../../../../payload/payload-types'
  
- const FooterComponent = ({footer}:{footer:Footer}) => {
+ const FooterComponent = () => {
   const pathname=usePathname()
 
-  const navItems=footer?.navItems || []
+  // const navItems=footer?.navItems || []
 
    return (
-     <footer className={noHeaderFooterUrls.includes(pathname)? classes.hide: ""}>
-      <Gutter>
+    <h1>Fppter</h1>
+    //  <footer className={noHeaderFooterUrls.includes(pathname)? classes.hide: ""}>
+    //   <Gutter>
 
-        {/* These inclusion is coming from the constants that we added */}
-        <ul className={classes.inclusions}>
-          {inclusions.map((inclusion,index)=>(
-            <li key={inclusion.title}>
-              <Image src={inclusion.icon} alt='inclusion.title' width={36} height={36} className={classes.icon}/>
-              <h5 className={classes.title}>{inclusion.title}</h5>
-              <p>{inclusion.description}</p>
-            </li>
-          ))}
-        </ul>
-      </Gutter>
+    //     {/* These inclusion is coming from the constants that we added */}
+    //     <ul className={classes.inclusions}>
+    //       {inclusions.map((inclusion,index)=>(
+    //         <li key={inclusion.title}>
+    //           <Image src={inclusion.icon} alt='inclusion.title' width={36} height={36} className={classes.icon}/>
+    //           <h5 className={classes.title}>{inclusion.title}</h5>
+    //           <p>{inclusion.description}</p>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </Gutter>
 
-      <div className={classes.footer}>
-        <Gutter>
-          <div className={classes.wrap}>
+    //   <div className={classes.footer}>
+    //     <Gutter>
+    //       <div className={classes.wrap}>
             
-            <Link href="/">
-              <Image src="/logo-white.svg" alt='logo' width={170} height={50}/>
-            </Link>
-            <p>{footer.copyright}</p>
+    //         <Link href="/">
+    //           <Image src="/logo-white.svg" alt='logo' width={170} height={50}/>
+    //         </Link>
+    //         <p>{footer.copyright}</p>
 
-            <div className={classes.socialLinks}>
-              {navItems.map((item)=>{
-                const icon= item?.link.icon as Media;
-                return (
-                  <Button 
-                  key={item.link.label}
-                  el='link'
-                  href={item.link.url}
-                  newTab={true}
-                  className={classes.socialLinkItem}
-                  >
-                    <Image
-                    src={icon?.url}
-                    alt={item.link.label}
-                    height={24}
-                    width={24}
-                    className={classes.socialIcon}/>
+    //         <div className={classes.socialLinks}>
+    //           {navItems.map((item)=>{
+    //             const icon= item?.link.icon as Media;
+    //             return (
+    //               <Button 
+    //               key={item.link.label}
+    //               el='link'
+    //               href={item.link.url}
+    //               newTab={true}
+    //               className={classes.socialLinkItem}
+    //               >
+    //                 <Image
+    //                 src={icon?.url}
+    //                 alt={item.link.label}
+    //                 height={24}
+    //                 width={24}
+    //                 className={classes.socialIcon}/>
                     
-                  </Button>
-                )
-              })}
-            </div>
-          </div>
-        </Gutter>
-      </div>
-     </footer>
+    //               </Button>
+    //             )
+    //           })}
+    //         </div>
+    //       </div>
+    //     </Gutter>
+    //   </div>
+    //  </footer>
    )
  }
  
